ca2bce59175b694f269aac634ffa6cba38647e7b
SPetrenko17 <SPetrenko17@yandex.ru>
2019-10-25 14:27:11 +0300

    first comit

---
+++ cmake-build-debug/CMakeFiles/Homework2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework2.dir/main.c.o"
+  "CMakeFiles/Homework2.dir/dynamic_library/dyn_lib.c.o"
+  "CMakeFiles/Homework2.dir/static_library/stat_lib.c.o"
+  "Homework2.pdb"
+  "Homework2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Homework2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
