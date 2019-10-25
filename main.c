5060fa12d6b8f0c9f96406d0011b5fa683aab0b8
SPetrenko17 <SPetrenko17@yandex.ru>
2019-10-25 19:09:25 +0300

    fix main

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "static_library/stat_lib.h"
 #include "dynamic_library/dyn_lib.h"
+void test();
 int main() {
     size_t len_max = 128;
     size_t seq_size = 0;
@@ -22,8 +24,13 @@ int main() {
     }
     sequence[i] = '\0';
     i++;
-    char* log_path = "./bible.txt";
-    mt_run(sequence, i, log_path);
-    run(sequence, i, log_path);
+    char* log_path = "./enwik8.txt";
+    int res_mt = mt_run(sequence, i, log_path);
+    int res_ot = run(sequence, i, log_path);
     return 0;
+}
+void test(char* log_path, char* sequence) {
+    int size = strlen(sequence);
+    int res_mt = mt_run(sequence, size, log_path);
+    int res_ot = run(sequence, size, log_path);
 }
