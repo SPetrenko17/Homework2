deb0d303a3b4e5231faae617e9699373328fb19f
SPetrenko17 <SPetrenko17@yandex.ru>
2019-10-26 12:53:57 +0300

    change main

--- main.c
+++ main.c
@@ -12,23 +12,25 @@ void tests();
 //void run();
 int main() {
     //tests();
+    // size_t len_max = 128;
+   // size_t seq_size = 0;
+    //char *sequence = malloc(len_max);
+    //seq_size = len_max;
+//    printf("\nEnter a string:");
+//    char c = EOF;
+//    size_t i = 0;
+//    while ((c = getchar()) != '\n' && c != EOF) {
+//        sequence[i++] = c;
+//        if (i == seq_size) {
+//            seq_size = i + len_max;
+//            sequence = realloc(sequence, seq_size);
+//        }
+//    }
+//    sequence[i] = '\0';
+//    i++;
 
-    size_t len_max = 128;
-    size_t seq_size = 0;
-    char *sequence = malloc(len_max);
-    seq_size = len_max;
-    printf("\nEnter a string:");
-    char c = EOF;
-    size_t i = 0;
-    while ((c = getchar()) != '\n' && c != EOF) {
-        sequence[i++] = c;
-        if (i == seq_size) {
-            seq_size = i + len_max;
-            sequence = realloc(sequence, seq_size);
-        }
-    }
-    sequence[i] = '\0';
-    i++;
+    char* sequence = "namespace";
+    int i = 9;
     char* log_path = "./enwik8.txt";
     int res_mt = mt_run(sequence, i, log_path);
     int res_ot = run(sequence, i, log_path);
