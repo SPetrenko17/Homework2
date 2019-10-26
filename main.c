1fed7acd9afcb5450e4fe6e8a03490dd3db5de04
SPetrenko17 <SPetrenko17@yandex.ru>
2019-10-26 11:25:19 +0300

    change main.c

--- main.c
+++ main.c
@@ -9,9 +9,26 @@
 #include "libdyn.h"//NOLINT
 void test(char* log_path, char* sequence, int answer);
 void tests();
+void run_user();
 int main() {
-    //tests();
+    tests();
+    return 0;
+}
 
+void tests(){
+    test("./enwik8.txt","namespace", 146);
+    test("./enwik8.txt","hello", 172);
+    test("./enwik8.txt","Ada programming language", 24);
+    test("./enwik8.txt","Random string in text doesn't exist", 0);
+}
+void test(char* log_path, char* sequence, int answer) {
+    int size = strlen(sequence);
+    int res_mt = mt_run(sequence, size, log_path);
+    int res_ot = run(sequence, size, log_path);
+    printf("%s %d %s %d", "test mt:", res_mt, " ot:", res_ot);
+    assert(res_mt == res_ot && res_mt == answer);
+}
+void run_user(){
     size_t len_max = 128;
     size_t seq_size = 0;
     char *sequence = malloc(len_max);
@@ -32,18 +49,4 @@ int main() {
     int res_mt = mt_run(sequence, i, log_path);
     int res_ot = run(sequence, i, log_path);
     printf("%s %d %s %d", "mt:", res_mt, " ot:", res_ot);
-    return 0;
-}
-void tests(){
-    test("./enwik8.txt","namespace", 146);
-    test("./enwik8.txt","hello", 172);
-    test("./enwik8.txt","Ada programming language", 24);
-    test("./enwik8.txt","Random string in text doesn't exist", 0);
-}
-void test(char* log_path, char* sequence, int answer) {
-    int size = strlen(sequence);
-    int res_mt = mt_run(sequence, size, log_path);
-    int res_ot = run(sequence, size, log_path);
-    printf("%s %d %s %d", "test mt:", res_mt, " ot:", res_ot);
-    assert(res_mt == res_ot && res_mt == answer);
 }
